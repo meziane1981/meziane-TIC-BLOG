7c32515dcb393a40ae8f0b4aa82743c35051e12d
MEZIANE <69803843+meziane1981@users.noreply.github.com>
2021-08-15 14:11:18 +0200

    frist commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# meziane-TIC-BLOG
---
+++ index.php
@@ -0,0 +1,54 @@
+  <?php
+require 'functions/main-functions.php';
+$pages = scandir('pages/');
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    if(in_array($_GET['page'].'.php',$pages)){
+        $page = $_GET['page'];
+    }else{
+        $page = "error";
+    }
+}else{
+    $page = "home";
+}
+
+$pages_functions = scandir('functions/');
+if(in_array($page.'.func.php',$pages_functions)){
+    require 'functions/'.$page.'.func.php';
+}
+?>
+  
+  
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+      <title>Blog MEZIANE DEV</title>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      
+            <?php
+            require "body/topbar.php";
+            ?>
+            <div class="container">
+            <?php
+                require 'pages/'.$page.'.php';
+            ?>
+            </div>
+
+
+
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+      <script type="text/javascript" src="js/materialize.js"></script>
+      <!-- <script type="text/javascript" src="js/script.js"></script> -->
+    </body>
+  </html>
+
