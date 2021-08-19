94dea67643678b0555e962af4c6481b2674d3d10
meziane1981 <meziane_maamar@yahoofr>
2021-08-19 19:14:00 +0200

    6 envoi

--- functions/post.func.php
+++ functions/post.func.php
@@ -1,23 +1,54 @@
-<?php
+    <?php
 
-function get_post(){
-    global $db;
+    function get_post(){
+        global $db;
 
-    $req = $db->query("
-        SELECT  posts.id,
-                posts.title,
-                posts.image,
-                posts.content,
-                posts.date,
-                admins.name
-        FROM posts
-        JOIN admins
-        ON posts.writer = admins.email
-        WHERE posts.id='{$_GET['id']}'
+        $req = $db->query("
+            SELECT  posts.id,
+                    posts.title,
+                    posts.image,
+                    posts.content,
+                    posts.date,
+                    admins.name
+            FROM posts
+            JOIN admins
+            ON posts.writer = admins.email
+            WHERE posts.id='{$_GET['id']}'
 
-    ");
+        ");
 
-    $result = $req->fetchObject();
-    return $result;
+        $result = $req->fetchObject();
+        return $result;
 
-}
+                                                }
+
+
+            function comment($name,$email,$comment){
+
+                global $db;
+
+                $c = array(
+                    'name'      => $name,
+                    'email'     => $email,
+                    'comment'   => $comment,
+                    'post_id'   => $_GET["id"]
+                );
+
+                $sql = "INSERT INTO comments(name,email,comment,post_id,date) VALUES(:name, :email, :comment, :post_id, NOW())";
+                $req = $db->prepare($sql);
+                $req->execute($c);
+
+                                                    }
+
+                function get_comments(){
+
+                global $db;
+                $req = $db->query("SELECT * FROM comments WHERE post_id = '{$_GET['id']}' ORDER BY date DESC");
+                $results = [];
+                while($rows = $req->fetchObject()){
+                    $results[] = $rows;
+                }
+
+                return $results;
+
+                                                }
--- pages/post.php
+++ pages/post.php
@@ -1,25 +1,64 @@
-<?php
+            <?php
 
-$post = get_post();
-if($post == false){
-    header("Location:index.php?page=error");
-}else{
-    ?>
-        </div>
-    <div class="parallax-container">
-        <div class="parallax">
-            <img src="img/posts/<?= $post->image ?>" alt="<?= $post->title ?>"/>
-        </div>
-    </div>
-        <div class="container">
-        <h2><?= $post->title ?></h2>
-        <h6>Par <?= $post->name ?> le <?= date("d/m/Y à H:i", strtotime($post->date)) ?></h6>
-            <p><?= nl2br($post->content); ?></p>
-     <?php      
-}
-?>
-<hr>
-            <h4>Commentaires:</h4>
+            $post = get_post();
+            if($post == false){
+                header("Location:index.php?page=error");
+            }else{
+                ?>
+                    </div>
+                <div class="parallax-container">
+                    <div class="parallax">
+                        <img src="img/posts/<?= $post->image ?>" alt="<?= $post->title ?>"/>
+                    </div>
+                </div>
+                    <div class="container">
+                    <h2><?= $post->title ?></h2>
+                    <h6>Par <?= $post->name ?> le <?= date("d/m/Y à H:i", strtotime($post->date)) ?></h6>
+                        <p><?= nl2br($post->content); ?></p>
+                <?php      
+            }
+            ?>
+            <hr>
+                    <h4>Commenter:</h4>
+                    <?php
+                        if(isset($_POST['submit'])){
+                            $name = htmlspecialchars(trim($_POST['name']));
+                            $email = htmlspecialchars(trim($_POST['email']));
+                            $comment = htmlspecialchars(trim($_POST['comment']));
+                            $errors = [];
+
+                            if(empty($name) || empty($email) || empty($comment)){
+                                $errors['empty'] = "Tous les champs n'ont pas été remplis";
+                            }else{
+                                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                                    $errors['email'] = "L'adresse email n'est pas valide";
+                                }
+                            }
+                            if(!empty($errors)){
+                                ?>
+                                    <div class="card red">
+                                        <div class="card-content white-text">
+                                            <?php
+                                                foreach($errors as $error){
+                                                    echo $error."<br/>";
+                                                }
+                                            ?>
+                                        </div>
+                                    </div>
+                                <?php
+                            }else{
+                                comment($name,$email,$comment);
+                                ?>
+                                    <script>
+                                        window.location.replace("index.php?page=post&id=<?= $_GET['id'] ?>");
+                                    </script>
+                                <?php
+                            }
+        
+                        }
+        
+                    ?>
+        
          
             <form method="post">
                 <div class="row">
