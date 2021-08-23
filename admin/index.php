a27547c1dac4a9d7d30c0b7e9ec5e53607fc74cd
meziane1981 <meziane_maamar@yahoofr>
2021-08-23 19:30:53 +0200

    11 envoie

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@ if(isset($_GET['page']) && !empty($_GET['page'])){
 
 $pages_functions = scandir('functions/');
 if(in_array($page.'.func.php',$pages_functions)){
-    require 'functions/'.$page.'.func.php';
+    require '.functions/'.$page.'.func.php';
 }
 ?>
   
@@ -34,7 +34,7 @@ if(in_array($page.'.func.php',$pages_functions)){
     <body>
       
             <?php
-            if($page != 'login' && !isset($_SESSION['admin'])){
+           if($page != 'login' && $page != 'new' && !isset($_SESSION['admin'])){
                 header("Location: index.php?page=login");
             }
             require "body/topbar.php";
