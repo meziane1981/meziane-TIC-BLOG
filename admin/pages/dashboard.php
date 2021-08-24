f5e1ebb390b77520bccbec0f1f69877b93b0a072
meziane1981 <meziane_maamar@yahoofr>
2021-08-24 23:23:03 +0200

    13 envoie le 24 /08/2021

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -1,7 +1,52 @@
+ 
+        <h2>Tableau de bord</h2>
+        <div class="row">
+
+            <?php
+
+           $tables = [
+                "Publications"      =>  "posts",
+                "Commentaires"      =>  "comments",
+                "Administrateurs"   =>  "admins"
+            ];
+
+
+            $colors = [
+                "posts"     =>  "green",
+                "comments"  =>  "red",
+                "admins"    =>  "blue"
+            ];
+
+             ?>
+
+          <?php
+
+            foreach($tables as $table_name => $table){
+                        ?>
+            <div class="col l4 m6 s12">
+                <div class="card">
+                        <div class="card-content <?=getColor($table,$colors)?> white-text">
+                            <span class="card-title"><?= $table_name ?></span>
+                            <?php $nbrInTable = inTable($table); ?>  
+                            <h4><?= $nbrInTable[0] ?></h4>
+                        </div>
+                </div>
+            </div>
+                <?php
+                }
+            
+    ?>
+
+    </div>
+
+
+
+
+
 <pre>
 
  <?php  
-session_destroy();
+// session_destroy();
 var_dump($_SESSION);
 ?>
 </pre>
