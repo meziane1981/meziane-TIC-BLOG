4446debada4d072f7451376b3a5d5919a3c53246
meziane1981 <meziane_maamar@yahoofr>
2021-08-25 22:54:56 +0200

    15 envoie soire 25/08/21 4 h

--- admin/functions/dashboard.func.php
+++ admin/functions/dashboard.func.php
@@ -13,3 +13,29 @@ function getColor($table,$colors){
         return "orange";
     }
 }
+
+
+function get_comments(){
+    global $db;
+
+    $req = $db->query("
+        SELECT  comments.id,
+                comments.name,
+                comments.email,
+                comments.date,
+                comments.post_id,
+                comments.comment,
+                posts.title
+        FROM comments
+        JOIN posts
+        ON comments.post_id = posts.id
+        WHERE comments.seen = '0'
+        ORDER BY comments.date ASC
+    ");
+
+    $results = [];
+    while($rows = $req->fetchObject()){
+        $results[] = $rows;
+    }
+    return $results;
+}
