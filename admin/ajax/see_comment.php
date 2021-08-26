25b5231b918554b05d85ca2861686909c4eda653
meziane1981 <meziane_maamar@yahoofr>
2021-08-26 21:51:44 +0200

    17 envoie le 26 08 21

---
+++ admin/ajax/see_comment.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require ('../../functions/main-functions.php');
+
+
+
+
+
+?>
--- admin/js/dashboard.func.js
+++ admin/js/dashboard.func.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    $('.modal').modal();
-});
+            $('.modal').modal();
+
+            $(".see_comment").click(function() {
+                var id = $(this).attr("id");
+
+                $.post('ajax/see_comment.php', { id: id }, function() {
+                    $("#commentaire_" + id).hide();
+                });
+            });
