6ec119ea0397641a9ba786e9c6d3536a76f4532c
meziane1981 <meziane_maamar@yahoofr>
2021-08-29 23:27:49 +0200

    20 envoi le 29/08/21 8 h

--- admin/pages/write.php
+++ admin/pages/write.php
@@ -1,26 +1,51 @@
 
 <h2>Poster un article</h2>
+
 <?php
-if(isset($_POST['post'])){
-    $title = htmlspecialchars(trim($_POST['title']));
-    $content = htmlspecialchars(trim($_POST['content']));
-    $posted = isset($_POST['public']) ? "1" : "0";
 
-    $errors = [];
-    if(empty($title) || empty($content)){
-        $errors['empty'] = "Veuillez remplir tous les champs";
+    if(isset($_POST['post'])){
+        $title = htmlspecialchars(trim($_POST['title']));
+        $content = htmlspecialchars(trim($_POST['content']));
+        $posted = isset($_POST['public']) ? "1" : "0";
+
+        $errors = [];
+
+        if(empty($title) || empty($content)){
+            $errors['empty'] = "Veuillez remplir tous les champs";
+        }
+
+        if(!empty($_FILES['image']['name'])){
+            $file = $_FILES['image']['name'];
+            $extensions = ['.png','.jpg','.jpeg','.gif','.PNG','.JPG','.JPEG','.GIF'];
+            $extension = strrchr($file,'.');
+
+            if(!in_array($extension,$extensions)){
+                $errors['image'] = "Cette image n'est pas valable";
+            }
+        }
+
+        if(!empty($errors)){
+            ?>
+                <div class="card red">
+                    <div class="card-content white-text">
+                        <?php
+                            foreach($errors as $error){
+                                echo $error."<br/>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            <?php
+        }else{
+            post($title,$content,$posted);
+            if(!empty($_FILES['image']['name'])){
+                post_img($_FILES['image']['tmp_name'], $extension);
+            }else{
+                $id = $db->lastInsertId();
+                header("Location:index.php?page=post&id=".$id);
+            }
+        }
     }
-
-    if(!empty($_FILES['image']['name'])){
-        $file = $_FILES['image']['name'];
-        $extensions = ['.png','.jpg','.jpeg','.gif','.PNG','.JPG','.JPEG','.GIF'];
-        $extension = strrchr($file,'.');
-        
-    
-    }
-
-}
-
 
 
 ?>
@@ -39,15 +64,21 @@ if(isset($_POST['post'])){
             <textarea name="content" id="content" class="materialize-textarea"></textarea>
             <label for="content">Contenu de l'article</label>
         </div>
-        <div class="col s12">
-            <div class="input-field file-field">
-                <div class="btn col s2">
-                    <span>Image de l'article</span>
-                    <input type="file" name="image" class="col s12"/>
-                </div class="file-path-wrapper">
-                <input type="text" class="file-path col s10" readonly/>
+
+    <div class="col s12">
+        <div class="file-field input-field">
+            <div class="btn col s2">
+                <span>Image de l'article</span>
+                <input type="file" name="image"/>
+            </div>
+            <div class="file-path-wrapper">
+                <input class="file-path validate col s10" type="text" readonly/>
             </div>
         </div>
+    </div>
+
+
+
 
         <div class="col s6">
             <p>Public</p>
@@ -71,3 +102,4 @@ if(isset($_POST['post'])){
 
 
 </form>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero odio nemo in necessitatibus laudantium dolor? Consectetur saepe nemo, ex reiciendis ducimus, perferendis voluptatibus dolor autem dicta facilis, accusantium quas magni.
